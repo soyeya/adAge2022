4bbefbbab6a05e63886ddea89ee85a7b87f5442a
soyeya <parksoye123@gmail.com>
2022-06-28 20:44:10 +0900

    first

---
+++ js/detail.js
@@ -0,0 +1,68 @@
+/*detail.js*/
+
+//2차 배열의 패턴 : ["이미지", "타이틀", "내용"]
+var $nike = [
+    ["nike_01.jpg", "nike_title_ad_1", "nike_text_ad_1"],
+    ["nike_02.jpg", "nike_title_ad_2", "nike_text_ad_2"],
+    ["nike_03.jpg", "nike_title_ad_3", "nike_text_ad_3"],
+    ["nike_04.jpg", "nike_title_ad_4", "nike_text_ad_4"],
+];
+var $adidas = [
+    ["adidas_01.jpg", "adidas_title_ad_1", "adidas_text_ad_1"],
+    ["adidas_02.jpg", "adidas_title_ad_2", "adidas_text_ad_2"],
+    ["adidas_03.jpg", "adidas_title_ad_3", "adidas_text_ad_3"],
+    ["adidas_04.jpg", "adidas_title_ad_4", "adidas_text_ad_4"],
+];
+var $puma = [
+    ["puma_01.jpg", "puma_title_ad_1", "puma_text_ad_1"],
+    ["puma_02.jpg", "puma_title_ad_2", "puma_text_ad_2"],
+    ["puma_03.jpg", "puma_title_ad_3", "puma_text_ad_3"],
+    ["puma_04.jpg", "puma_title_ad_4", "puma_text_ad_4"],
+];
+
+//http://127.0.0.1:5500/detail.html#nike_1
+
+const $hash = location.hash;
+console.log($hash); //nike_1 ==> nike:배열로 구분하여 접근하는 용도/ 1:각 1차 배열의 인덱스 번호로 접근
+
+const $hash_txt = $hash.replace("#","");
+console.log($hash_txt); //nike_1
+//nike_1 ==(변수.split("특정문자"))==> ["nike", "1"]
+const $divide_arr = $hash_txt.split("_");
+console.log($divide_arr); // ['nike', '1']
+const $brand = $divide_arr[0]; //카테고리 정의(nike | adidas | puma)
+const $index = $divide_arr[1]; //각 카테고리별로 데이터의 인덱스번호(0 | 1 | 2 | 3)
+
+
+//선택자 구성 
+const $detailBg = document.querySelector("#cont .ad_img");
+const $detailTitle = document.querySelector("#cont h3");
+const $detailText = document.querySelector("#cont p");
+
+if($brand == "nike"){
+    $detailBg.style.backgroundImage = `url(./img/${$nike[$index][0]})`;
+    $detailTitle.textContent = $nike[$index][1];
+    $detailText.textContent = $nike[$index][2];
+}
+
+if($brand == "adidas"){
+    $detailBg.style.backgroundImage = `url(./img/${$adidas[$index][0]})`;
+    $detailTitle.textContent = $adidas[$index][1];
+    $detailText.textContent = $adidas[$index][2];
+}
+if($brand == "puma"){
+    $detailBg.style.backgroundImage = `url(./img/${$puma[$index][0]})`;
+    $detailTitle.textContent = $puma[$index][1];
+    $detailText.textContent = $puma[$index][2];
+}
+
+//하단의 목록보기 클릭시 
+
+const backBtn = document.querySelector(".history button");
+
+backBtn.addEventListener("click", () => {
+ //history.forward(); //방문기록상 한단계 앞으로 진행한다.
+ //history.back(); //방문기록상 한단계 뒤로 진행한다.
+ //history.go(-2);  //방문기록상 두단계 뒤로 진행한다.   
+ location.href=`./sub.html#${$brand}`;
+});
---
+++ js/sub.js
@@ -0,0 +1,62 @@
+/*sub.js*/
+//http://127.0.0.1:5500/sub.html#nike
+
+const nikeTxt = "Just Do it";
+const adidasTxt = "Impossible if Nothing - adidas";
+const pumaTxt = "Foreverfaster - puma";
+
+//index.html -> sub.html 진입했을 때
+const $hash = location.hash;  //URL로부터 #을 포함한 우측의 문구를 가져오기
+console.log($hash);  //#nike
+let $hash_txt = $hash.replace("#", "");
+console.log($hash_txt);  //nike
+
+//선택자 구성하기
+const $main = document.querySelector("main");  //배너이미지 교체
+const $title = document.querySelector("#cont .wrap .title h3");  //페이지의 타이틀 교체
+const $contCover = document.querySelector("#cont .wrap .cont");  //내부 이미지를 담을 공간
+const $prTxt = document.querySelector(".presentiveTxt");
+let $contGroup = "";  //.cont라는 공간에 넣을 가상의 빈 문자열 데이터
+
+const hash_apply = () => {
+    if($hash_txt){  //hash라는 정보가 존재한다면
+        $main.style.backgroundImage = `url(./img/${$hash_txt}_banner.jpg)`;
+        $title.textContent = $hash_txt;
+        $contGroup = ""; //기존에 쌓인 모든 콘텐츠를 제거하여 다시 넣는다.
+
+        for(i = 0; i < 4; i++){  //i = 0 -> 1 -> 2 -> 3
+            $contGroup += `<div style="background-image:url(./img/${$hash_txt}_0${i+1}.jpg)" onclick="location.href='./detail.html#${$hash_txt}_${i}'"></div>`;
+        }
+        $contCover.innerHTML = $contGroup;
+
+        if($hash_txt == "nike"){
+        $prTxt.textContent = nikeTxt;
+        }else if($hash_txt == "adidas"){
+        $prTxt.textContent = adidasTxt;
+        }else if($hash_txt == "puma"){
+            $prTxt.textContent = pumaTxt;
+            }
+
+    }else{//hash라는 정보가 존재하지 않다면 => index.html로 보낸다.
+        location.href="./";  //index.html로 강제 이동시키기
+    }
+}
+
+hash_apply();
+
+
+//sub.html 페이지에서 상단의 메뉴 클릭시 
+
+const $list = document.querySelectorAll("header .wrap nav ul li");
+console.log($list);
+
+for(const v of $list){//const 예약어를 사용하면 요소의 위치를 고정화시키는 역할로 담당
+    console.log(v);
+    v.addEventListener("click", () => {
+     $hash_txt = v.getAttribute("rel"); //기존 해시로부터 받아온 값에서 rel에 담겨진 속성을 가지고 속성값으로 변경
+     hash_apply();
+});
+}
+
+//================================================================0628===============================================
+
